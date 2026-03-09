9eeb9c699c655f7e10bb8e1d2dda7882769a845b
Tonkonto <vasilii.mh.worker@gmail.com>
2026-03-09 16:10:23 +0600

    stage B

---
+++ GeoComponent/Models/GeoMapComponentModel.cs
@@ -0,0 +1,10 @@
+﻿using GeoComponent.Core.Models;
+
+namespace GeoComponent.Models;
+
+public class GeoMapComponentModel
+{
+    public GeoResult? Geo { get; set; }
+    public IEnumerable<GeoResult>? GeoBatch { get; set; }
+    public GeoMapOptions Options { get; set; } = new();
+}
--- GeoComponent/ViewComponents/GeoMapViewComponent.cs
+++ GeoComponent/ViewComponents/GeoMapViewComponent.cs
@@ -1,21 +1,19 @@
-﻿using GeoComponent.Core.Models;
-using GeoComponent.Models;
+﻿using GeoComponent.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeoComponent.ViewComponents;
 
 public class GeoMapViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke(
-        GeoResult? geo = null,
-        IEnumerable<GeoResult>? geoBatch = null,
-        GeoMapOptions? options = null )
+    public IViewComponentResult Invoke(GeoMapComponentModel model)
     {
+        ArgumentNullException.ThrowIfNull(model);
+
         var vm = new GeoMapVm
         {
-            Geo = geo,
-            GeoBatch = geoBatch,
-            Options = options ?? new GeoMapOptions()
+            Geo = model.Geo,
+            GeoBatch = model.GeoBatch,
+            Options = model.Options ?? new GeoMapOptions()
         };
 
         return View("~/Views/Shared/Components/GeoMap/Default.cshtml", vm);
